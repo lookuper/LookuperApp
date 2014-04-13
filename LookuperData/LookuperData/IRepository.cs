45b35c3b170c46c5bed40eb146b8e797dc7e022e
Maksym Chernenko <Maksym.Chernenko@gmail.com>
2014-04-13 16:23:06 +0300

    LookuperDatat project initial commit

---
+++ LookuperData/LookuperData/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookuperData
+{
+    public interface IRepository<T> where T : class
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> GetAll();
+
+        void Save();
+    }
+}
