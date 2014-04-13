ff5bc284d6d3497b140c2e01787988ff975fb0bc
Maksym Chernenko <Maksym.Chernenko@gmail.com>
2014-04-13 22:16:47 +0300

    add UI project, without browser dependencie

---
+++ LookuperView/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace LookuperView
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void ApplicationDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var error = e.Exception;
+            var formattedError = String.Format("{0} | {1}", error.Source, error.Message);
+            var mainWindow = Application.Current.MainWindow;
+
+            var viewModel = mainWindow.DataContext as LookuperViewModel;
+            if (viewModel != null)
+            {
+                viewModel.SetError(formattedError, error.StackTrace);
+            }
+            else
+            {
+                var inputFormViewModel = mainWindow.DataContext as InputFormViewModel;
+                if (inputFormViewModel != null)
+                {
+                    //inputFormViewModel.ShowError(formattedError, error.StackTrace);
+                }
+            }
+
+            e.Handled = true;
+        }
+    }
+}
---
+++ LookuperView/LookuperViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookuperView
+{
+    public class LookuperViewModel
+    {
+        internal void SetError(string formattedError, string p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
