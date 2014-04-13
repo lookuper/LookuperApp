ef62edc11694205d9699e0c3f1ba1090baaad2b5
Maksym Chernenko <Maksym.Chernenko@gmail.com>
2014-04-13 16:39:19 +0300

    add LookuperData project

---
+++ LookupperData/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookupperData
+{
+    public interface IRepository<T> where T : class
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> GetAll();
+
+        void Save();
+    }
+}
