ce00b4a51c47cdba86157b7851acc19db554c16e
Maksym Chernenko <Maksym.Chernenko@gmail.com>
2014-04-16 22:12:54 +0300

    Changes in dataModel

---
+++ LookuperModel/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookuperModel
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            var model = new LookuperModel();
+            var data = model.AvaliableLookupItems;
+        }
+    }
+}
